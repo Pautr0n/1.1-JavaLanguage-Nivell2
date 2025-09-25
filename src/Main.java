134330afae7a87bed27e2ec679d974f5276183cd
PauG <paugaston@hotmail.com>
2025-09-25 14:06:55 +0200

    Main completed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 void main() {
+
+    Smartphone smartphoneA = new Smartphone("Iphone", "16");
+
+    smartphoneA.makePhoneCall(666775577);
+
+    smartphoneA.takePicture();
+    smartphoneA.ringAlarm();
+
+    Smartphone smartphoneB = new Smartphone("Samsung", "Galaxy Ultra 24");
+
+    smartphoneB.makePhoneCall(665899899);
+
+    smartphoneB.takePicture();
+    smartphoneB.ringAlarm();
+
 }
